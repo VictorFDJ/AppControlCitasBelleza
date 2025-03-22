32c47b0b371356e5f09cfe961d53282df51e5411
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-03-22 11:40:55 -0400

    He creado la migracion de mi base de datos

---
+++ AppControlBelleza/ControlBelleza.Frontend/Migrations/20250322152932_migracion_estilista.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControlBelleza.Frontend.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracion_estilista : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estilista",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Gmail = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estilista", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estilista");
+        }
+    }
+}
