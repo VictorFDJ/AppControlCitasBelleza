f5d79df630801d50eb7cca668534cfe3c8f51abd
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-04-12 01:25:54 -0400

    he creado los modelos

---
+++ AppControlBelleza/ControlBelleza.Frontend/Models/EstilistaModel.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.AspNetCore.Http.HttpResults;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ControlBelleza.Domain.Entities
+{
+    public class Estilista
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [StringLength(50)]
+        public string Apellido { get; set; }
+        [StringLength(75)]
+        public string Gmail { get; set; }
+        [StringLength(25)]
+        public string Telefono { get; set; }
+
+    }
+
+
+}
+
+
