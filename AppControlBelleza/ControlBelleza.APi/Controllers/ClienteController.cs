f21b90691d16c015cf13f7dcff7612208c5926fd
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-04-12 02:01:27 -0400

    he actualizado el controlador de cita

--- AppControlBelleza/ControlBelleza.APi/Controllers/ClienteController.cs
+++ AppControlBelleza/ControlBelleza.APi/Controllers/ClienteController.cs
@@ -1,18 +1,75 @@
-using ControlBelleza.ControlBelleza.Persitence;
 using Microsoft.AspNetCore.Mvc;
-
-namespace ControlBelleza.APi.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class ClienteController : ControllerBase
+using Microsoft.EntityFrameworkCore;
+//using CRUD.Persitense;
+using ControlBelleza.Domain.Entities;
+using ControlBelleza.ControlBelleza.Persitence;
+using ControlBelleza.Domain.Entities;
+namespace proyecto.api.Controllers
 {
-    private readonly DataContext _context;
-    public ClienteController(DataContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
     {
-        _context = context;
+        private readonly DataContext _context;
+
+        public ClientesController(DataContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("Crear")]
+        public async Task<IActionResult> CrearCliente(Cliente cliente)
+        {
+            await _context.Clientes.AddAsync(cliente);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+        [HttpGet]
+        [Route("lista")]
+        public async Task<ActionResult<IEnumerable<Cliente>>> ListaClientes()
+        {
+
+            var clientes = await _context.Clientes.ToListAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet]
+        [Route("Buscar por ID")]
+
+        public async Task<IActionResult> VerCliente(int id)
+        {
+            Cliente cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
+        [HttpPut]
+        [Route("editar")]
+        public async Task<IActionResult> ActualizarCliente(int id, Cliente cliente)
+        {
+            var clientes = await _context.Clientes.FindAsync(id);
+
+            clientes!.Name = cliente.Name;
+            //clientes.Name = cliente.Telefono;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("eliminar")]
+        public async Task<IActionResult> EliminarCliente(int id)
+        {
+            var ClienteRemove = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(ClienteRemove!);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
-
-
-
 }
