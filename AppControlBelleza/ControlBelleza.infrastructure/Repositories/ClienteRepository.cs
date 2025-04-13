f9656da0d8f350d561b5bff5b54785db52c36566
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-04-13 15:40:48 -0400

    cambios realizados

---
+++ AppControlBelleza/ControlBelleza.infrastructure/Repositories/ClienteRepository.cs
@@ -0,0 +1,19 @@
+﻿using ControlBelleza.ControlBelleza.Persitence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBelleza.infrastructure.Repositories
+{
+    class ClienteRepository
+    {
+        private readonly DataContext _context;
+
+        public ClienteRepository(DataContext context)
+        {
+            this._context = context;
+        }
+    }
+}
