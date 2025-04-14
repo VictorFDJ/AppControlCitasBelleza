acc6965361df5891db461c0b444647a6e85b762b
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-04-14 00:09:49 -0400

    cambios realizados en los models

--- AppControlBelleza/ControlBelleza.infrastructure/Repositories/ClienteRepository.cs
+++ AppControlBelleza/ControlBelleza.infrastructure/Repositories/ClienteRepository.cs
@@ -1,7 +1,12 @@
 ﻿using ControlBelleza.ControlBelleza.Persitence;
+using ControlBelleza.Domain.Entities;
+using ControlBelleza.infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +19,59 @@ namespace ControlBelleza.infrastructure.Repositories
         public ClienteRepository(DataContext context)
         {
             this._context = context;
+        }
+    
+        public class ClientesController : ControllerBase
+        {
+            private readonly DataContext _context;
+
+            public ClientesController(DataContext context)
+            {
+
+                _context = context;
+            }
+
+         
+            //public async Task<ClienteModel> CrearCliente(Cliente cliente)
+            //{
+            //    await _context.Clientes.AddAsync(cliente);
+            //    await _context.SaveChangesAsync();
+            //    return Ok();
+
+            //}
+   
+            public async Task<ClienteModel> ListaClientes(ClienteModel ok)
+            {
+
+                var clientes = await _context.Clientes.ToListAsync();
+                return ok;
+            }
+
+    
+
+            //public async Task<ClienteModel> VerCliente(int id)
+            //{
+            //    Cliente cliente = await _context.Clientes.FindAsync(id);
+
+            //    if (cliente == null)
+            //    {
+            //        return NotFound();
+            //    }
+            //    return cliente
+            //}
+
+            //public async Task<ClienteModel> ActualizarCliente(int id, Cliente cliente)
+            //{
+            //    var clientes = await _context.Clientes.FindAsync(id);
+
+            //    clientes!.Name = cliente.Name;
+            //    //clientes.Name = cliente.Telefono;
+            //    await _context.SaveChangesAsync();
+
+            //    return Ok();
+            //}
+
+         
         }
     }
 }
